7fd7c4ba2a726cfadbe6c4d7e4aaa3e9a010610f
Saurabh Uchil <uchilsaurabh@gmail.com>
2026-03-23 22:24:13 +1100

    installed jest and a test file

---
+++ index.test.js
@@ -0,0 +1,11 @@
+describe('sum of two numbers', () => {
+    test('adds 1 + 2 to equal 3', () => {
+        expect(1 + 2).toBe(3);
+    });
+    test('adds 5 + 7 to equal 12', () => {
+        expect(5 + 7).toBe(12);
+    });
+    test('adds -1 + 1 to equal 0', () => {
+        expect(-1 + 0).toBe(0);
+    });
+});
